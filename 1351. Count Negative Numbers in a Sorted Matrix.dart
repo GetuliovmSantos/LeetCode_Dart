8142acd1493c929d4a511dbc60de2ecec42a044a
GetuliovmSantos <getulio.vm.santos@gmail.com>
2024-11-03 17:28:02 -0300

    1351. Count Negative Numbers in a Sorted Matrix .2
    adição de comentarios e remoção do metodo main

--- 1351. Count Negative Numbers in a Sorted Matrix.dart
+++ 1351. Count Negative Numbers in a Sorted Matrix.dart
@@ -1,24 +1,17 @@
 class Solution {
+  // Função para contar números negativos em uma matriz ordenada
   int countNegatives(List<List<int>> grid) {
-    int count = 0;
+    int count = 0; // Inicializa o contador de números negativos
 
+    // Itera sobre cada linha da matriz
     for (var i in grid) {
+      // Itera sobre cada elemento da linha
       for (var j in i) {
+        // Verifica se o elemento é negativo
         if (j < 0) count++;
       }
     }
 
-    return count;
+    return count; // Retorna o número total de elementos negativos
   }
-}
-
-void main(List<String> args) {
-  Solution s = Solution();
-
-  print(s.countNegatives([
-    [4, 3, 2, -1],
-    [3, 2, 1, -1],
-    [1, 1, -1, -2],
-    [-1, -1, -2, -3]
-  ]));
 }
